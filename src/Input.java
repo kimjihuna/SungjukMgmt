287ca486819114e3c59b4a151c91cf594210241b
User <User@DESKTOP-3F8JG65>
2022-08-08 09:03:45 +0900

    addd

--- src/Input.java
+++ src/Input.java
@@ -1,12 +1,41 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Input {
 	private Student [] array;
 	private Scanner sc;
+	private File file;
+	private BufferedReader br;
 	
 	public Input(Student[] array) {
 		this.array = array;
 		this.sc = new Scanner(System.in);
+		this.file = new File("C:/Temp/sungjuk_utf8.dat");
+		try {
+			this.br = new BufferedReader(new FileReader(this.file));
+		} catch (FileNotFoundException e) {
+			System.out.println("file not found");
+		}
+	}
+	public int fileInput() {
+		int count = 0;
+		String line = null;
+		while(true) {
+			count++;
+			try {
+				line = this.br.readLine();
+				System.out.println(line);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if(line==null)break;
+		}
+		return count-1;
 	}
 
 
--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,17 @@ public class Main {
 	public static void main(String[] args) {
 		Student[] array = new Student[100];
 		Input input = new Input(array);
-		int count = input.input();
-		
-		Calc calc = new Calc(array);
-		calc.calc(count);
-		
-		Sort sort = new Sort(array, count);
-		sort.bubbleSort();
-		
-		Output output = new Output();
-		output.print(array, count);
-		System.out.println("성적관리프로그램 끝");
-	}
+		int count = input.fileInput();
+		System.out.println("count = " + count);
 
+//		Calc calc = new Calc(array);
+//		calc.calc(count);
+//		
+//		Sort sort = new Sort(array, count);
+//		sort.bubbleSort();
+//		
+//		Output output = new Output();
+//		output.print(array, count);
+//		System.out.println("성적관리프로그램 끝");
+	}
 }
